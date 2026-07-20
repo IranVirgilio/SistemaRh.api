66185d6194c429aa1d199f42970bc126bed4287a
Iran_Virgilio <152872691+IranVirgilio@users.noreply.github.com>
2026-07-20 20:14:58 -0300

    Validação de dados
    Foi realizada o tratamentos de dados para que a api rejeite pedidos invalidos, protegendo o banco de dados .

--- SistemaRh.api/Models/Funcionario.cs
+++ SistemaRh.api/Models/Funcionario.cs
@@ -1,17 +1,26 @@
-﻿using System.ComponentModel.DataAnnotations.Schema; // nescessario adicionar essa linha para
-                                                    // tratar erro na criação do banco de dados
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // nescessario adicionar essa linha para  tratar erro na criação do banco de dados
+
+
 namespace SistemaRh.api.Models
 {
     public class Funcionario
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Cargo { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é Obrigatório!.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome deve ter entre 3 a 100 caracteres")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "O campo é Obrigatório!.")]
+        public string Cargo { get; set; } = string.Empty;
+
         public DateTime DataAdmissao { get; set; }
 
-        [Column (TypeName = "decimal(18,2)")] // <= delimitando o tamanho do valor para o banco
-                                            // e avisando sobre os centavos
+        [Column (TypeName = "decimal(18,2)")] // <= delimitando o tamanho do valor para o banco e avisando sobre os centavos
+        [Range(0.01, 500000.00, ErrorMessage = "Digite um valor de salário válido!.")] // <- Exige que o salário seja maior que zero.
         public decimal Salario { get; set; }
+
         public bool Ativo { get; set; }
     }
     
